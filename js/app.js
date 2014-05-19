6e84182191c987ee84d7e2801398af13d3bbbb35
Aswin <aswin@aswin-Inspiron-5420.(none)>
2014-05-19 10:38:34 +0530

    First commit

--- js/app.js
+++ js/app.js
@@ -1,57 +1,53 @@
 App = Ember.Application.create();
+
  
  
 App.Router.map(function(){
-this.resource('movies',function(){
-this.resource('createmovies', { path: 'new'});
-this.resource('listmovies', {path: 'list'});
+this.resource('movie',function(){
+this.resource('createmovie', { path: 'new'});
+this.resource('listmovie', {path: 'list'});
 });
  
 });
  
-App.Store = DS.Store.extend({
-revision: 1,
-url: "http://localhost:3000/api/v1"
+ 
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
+	host: "http://localhost:3000",
+	namespace: "api/v1"
+	
+	
+});
+ 
+
+App.Movie = DS.Model.extend({
+	name: DS.attr('string'),
+	description: DS.attr('string'),
+	length: DS.attr('number')
 });
  
  
- 
- 
- 
-App.CreatemoviesController = Ember.ObjectController.extend({
+App.CreatemovieController = Ember.ObjectController.extend({
 actions: {
 createmovie: function() {
-console.log('Creating movie');
-Ember.$.post("http://localhost:3000/api/v1/movies/create").done(
-function(res,status){
-console.log(status);
- 
-})
-.fail(
-function(res,status){
-console.log('Error');
-console.log(res);
-}
-);
+	var x = this.store.createRecord('movies',{
+		name:'hi',
+	    description: 'hello',
+	    length: 45
+	});
+	x.save();
 }
 }
  
 });
  
  
-App.MoviesController = Ember.ArrayController.extend();
- 
-var h= [{
-name: "Hi"
-},
-{
-name: "Hello"
-}
-];
+App.MovieController = Ember.ArrayController.extend();
  
  
-App.MoviesRoute = Ember.Route.extend({
+ 
+App.MovieRoute = Ember.Route.extend({
 model: function(){
-return h;
+var x = this.store.find('movie');
+	return x;
 }
 });
