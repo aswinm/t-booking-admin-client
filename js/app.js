a2ee31f6af8e0fdb59f2b03e802abf01465847ab
Aswin <aswin@aswin-Inspiron-5420.(none)>
2014-05-20 22:58:11 +0530

    CRUD movies enabled

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ App = Ember.Application.create();
  
  
 App.Router.map(function(){
-this.resource('movie',function(){
+this.resource('movies',function(){
 this.resource('createmovie', { path: 'new'});
 this.resource('listmovie', {path: 'list'});
+this.resource('movie', {path: ':movie_id'});
 });
  
 });
@@ -13,7 +14,8 @@ this.resource('listmovie', {path: 'list'});
  
 App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
 	host: "http://localhost:3000",
-	namespace: "api/v1"
+	namespace: "api/v1",
+	corsWithCredentials: true
 	
 	
 });
@@ -22,32 +24,70 @@ App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
 App.Movie = DS.Model.extend({
 	name: DS.attr('string'),
 	description: DS.attr('string'),
-	length: DS.attr('number')
+	length_minutes: DS.attr('number')
 });
  
  
 App.CreatemovieController = Ember.ObjectController.extend({
+	model: {},
 actions: {
-createmovie: function() {
-	var x = this.store.createRecord('movies',{
-		name:'hi',
-	    description: 'hello',
-	    length: 45
-	});
-	x.save();
+createmovie: function(data) {
+	console.log(this.get('name'));
+	$.get("http://localhost:3000/api/v1/movies/create/",{name: this.get('name'),description: this.get('description'),length: this.get('length')}).done(function(res){
+		alert("Created movie");});
 }
 }
  
 });
  
  
-App.MovieController = Ember.ArrayController.extend();
+App.ListmovieController = Ember.ArrayController.extend();
  
  
  
-App.MovieRoute = Ember.Route.extend({
+var x;
+App.ListmovieRoute = Ember.Route.extend({
 model: function(){
-var x = this.store.find('movie');
+x = this.store.find('movie');
 	return x;
 }
 });
+
+
+App.MovieRoute = Ember.Route.extend({
+	model: function(){
+		var z= this.store.find('movie',params.movie_id);
+		console.log(z);
+		return z;
+	},
+	reloadmodel: function(){
+		this.get('model').reload();
+	}
+	
+});
+
+App.MovieController = Ember.ObjectController.extend({
+	model: {},
+	isEditing: false,
+	actions: {
+		edited: function(data){
+			this.set('isEditing',false);
+			$.get("http://localhost:3000/api/v1/movies/update",{name: this.get('name'),description:this.get('description'),length:this.get('length_minutes'),id:this.get('id')}).done(function(res){
+				alert('Edited successfully');
+			});
+		},
+		edit: function(data){
+			this.set('isEditing',true);
+			console.log('Editing started');
+		},
+		del: function(data){
+			$.get("http://localhost:3000/api/v1/movies/delete",{ id:this.get('id')}).done(function(res){
+				alert('Deleted successfully');
+				this.get('model').reload();
+			});
+		}
+	}
+
+});
+		
+
